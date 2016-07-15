54509437e06b70db5fa0cbab36391b7894b04429
tjlpow <tjlevil@gmail.com>
2016-07-15 17:21:37 +0800

    2

---
+++ AS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AS
+//
+//  Created by baidu on 16/7/15.
+//  Copyright © 2016年 Alan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
